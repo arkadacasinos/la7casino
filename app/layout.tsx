c5d601c960cfdfef59747aeca5814321b51bba6c
arkadacasinos <bezdep.rf@mail.ru>
2026-09-17 22:08:49 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${display.variable} ${body.variable}`}>
       <head>
+        <meta name="yandex-verification" content="12679cd88c8ef234" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="La7Casino" />
